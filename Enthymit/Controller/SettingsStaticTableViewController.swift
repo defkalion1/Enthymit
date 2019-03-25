14d7d895aa4af2e0e01d882b9a609ccf5cee6e9f
Constantine Defkalion Politopoulos <defkalion@me.com>
2019-03-25 21:13:15 +0200

    Can save the state of the UISwitch in settings using user defaults

--- Enthymit/Controller/SettingsStaticTableViewController.swift
+++ Enthymit/Controller/SettingsStaticTableViewController.swift
@@ -9,82 +9,34 @@
 import UIKit
 
 class SettingsStaticTableViewController: UITableViewController {
+    
+    
+    
+    @IBOutlet weak var darkTheme: UISwitch!
+    @IBOutlet weak var securityUnlock: UISwitch!
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        darkTheme.isOn = defaults.bool(forKey: "DarkThemeIsOn")
+        securityUnlock.isOn = defaults.bool(forKey: "AuthIsOn")
 
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+    
+    @IBAction func darkThemePressed(_ sender: UISwitch) {
+        //self.defaults.set(darkThemeOn, forKey: "DarkThemeOn")
+        self.defaults.set(darkTheme.isOn, forKey: "DarkThemeIsOn")
+        
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    @IBAction func authPressed(_ sender: UISwitch) {
+        self.defaults.set(securityUnlock.isOn, forKey: "AuthIsOn")
+        
+        
     }
-    */
-
+    
+    
 }
